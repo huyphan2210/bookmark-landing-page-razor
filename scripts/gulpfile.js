fd8231f12a216dafe7fe007e9efd2a6651777d34
Huy Phan <huy.phan2210@outlook.com>
2024-08-01 11:46:55 +0700

    Use gulp + sass to auto generate css files

---
+++ scripts/gulpfile.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const { task, series, src, dest, watch } = require("gulp");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("sass"));
+
+// Change working directory to the parent of the 'scripts' folder
+process.chdir(path.resolve(__dirname, ".."));
+
+task("sass", function () {
+  return src("**/*.razor.scss") // Source folder
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest(path.resolve(__dirname, '..')));
+});
+
+task("watch", function () {
+  watch("**/*.razor.scss", series("sass"));
+});
+
+task("default", series("sass", "watch"));
